14411176706a46eafd16fbd1501cdddd84baa168
Tomasz Gawe <tomincjusz@gmail.com>
2022-04-19 18:54:26 +0200

    kodilla 19.1

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-  const apiRoot = 'http://kodilla24.herokuapp.com/v1/tasks';
-  const trelloApiRoot = 'http://kodilla24.herokuapp.com/v1/trello';
+  const apiRoot = 'https://kodilla24.herokuapp.com/v1/tasks';
+  const trelloApiRoot = 'https://kodilla24.herokuapp.com/v1/trello';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
